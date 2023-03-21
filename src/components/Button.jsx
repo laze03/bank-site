0f7ba5c637be8207f8412cca33def3b3bb9cbbad
laze03 <Lazrak.edu@gmail.com>
2023-03-21 00:58:53 +0000

    commiting the bank site

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-md `}
+  >
+    Get started
+  </button>
+);
+
+export default Button;
